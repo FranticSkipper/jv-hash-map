a91a051e326598ca77acbb9cdcecb98304eaa4c0
Oleg <oleg@gmail.com>
2025-09-22 17:56:53 +0300

    implement custom hashmap class

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,19 +1,124 @@
 package core.basesyntax;
 
+import java.util.Objects;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static final int INITIAL_CAPACITY = 16;
+    private static final float LOAD_FACTOR = 0.75f;
+    private int capacity = INITIAL_CAPACITY;
+    private int size = 0;
+    private Node<K, V>[] elements = new Node[capacity];
+
+    public MyHashMap() {
+    }
 
     @Override
     public void put(K key, V value) {
+        int hash = this.hash(key);
+        int position = hash % this.capacity;
+        Node<K, V> currentNode = this.getNode(key);
 
+        if (currentNode == null) {
+            this.elements[position] = new Node<K, V>(hash, key, value, null);
+        } else {
+            Node<K, V> lastNode = null;
+
+            while (currentNode != null) {
+                if (Objects.equals(key, currentNode.key)) {
+                    currentNode.value = value;
+                    return;
+                }
+
+                lastNode = currentNode;
+                currentNode = currentNode.next;
+            }
+
+            lastNode.next = new Node<K, V>(hash, key, value, null);
+        }
+
+        if (++this.size > (int)(capacity * LOAD_FACTOR)) {
+            this.resize();
+        }
     }
 
     @Override
     public V getValue(K key) {
-        return null;
+        Node<K, V> currentNode = this.getNode(key);
+
+        if (currentNode == null) {
+            return null;
+        }
+
+        while (currentNode != null) {
+            if (Objects.equals(currentNode.key, key)) {
+                break;
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return currentNode == null ? null : currentNode.value;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return this.size;
+    }
+
+    private void resize() {
+        int oldCap = this.capacity;
+        int newCap = this.capacity << 1;
+        Node<K, V>[] newElements = new Node[newCap];
+
+        for (int i = 0; i < oldCap; i++) {
+            Node<K, V> node = this.elements[i];
+
+            while (node != null) {
+                int newNodeHash = this.hash(node.key);
+                int newPosition = newNodeHash % newCap;
+                Node<K, V> newNode = new Node<>(node.hash, node.key, node.value, null);
+
+                if (newElements[newPosition] == null) {
+                    newElements[newPosition] = newNode;
+                } else {
+                    Node<K, V> currentNode = newElements[newPosition];
+                    Node<K, V> lastNode = null;
+
+                    while (currentNode != null) {
+                        lastNode = currentNode;
+                        currentNode = currentNode.next;
+                    }
+
+                    lastNode.next = newNode;
+                }
+
+                node = node.next;
+            }
+        }
+
+        this.elements = newElements;
+        this.capacity = newCap;
+    }
+
+    private int hash(K key) {
+        return key == null ? 0 : Math.abs(key.hashCode());
+    }
+
+    private Node<K, V> getNode(K key) {
+        return this.elements[this.hash(key) % this.capacity];
+    }
+
+    private static class Node<K, V> {
+        private final int hash;
+        private final K key;
+        private V value;
+        private Node<K, V> next;
+
+        public Node(int hash, K key, V value, Node<K, V> node) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = node;
+        }
     }
 }
